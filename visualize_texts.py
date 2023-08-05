1fa07e00cf5473436ddc6c39b905f0163de9e379
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-08-05 11:50:58 +0200

    feat: code commentary

--- visualize_texts.py
+++ visualize_texts.py
@@ -24,20 +24,23 @@ def word_cloud(tokens, file_name):
     This function plots a word cloud of the tokens.
     cf. https://towardsdatascience.com/end-to-end-topic-modeling-in-python-latent-dirichlet-allocation-lda-35ce4ed6b3e0
     '''
+    # possible to add stopwords to initiation of worldcloud
     wordcloud = WordCloud(width=800, height=500, random_state=21, contour_width=3, max_font_size=110, background_color='white', max_words=5000).generate(','.join(tokens))
     plt.figure(figsize=(15, 10))
-    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.imshow(wordcloud, interpolation="bilinear") # displays image, interpolation: smoother image
     plt.axis('off')
     plt.title('Word cloud in ' + file_name)
     plt.show()
 
+
 if __name__ == '__main__':
-    print('Visualize texts')
     for path in glob.glob('/Users/klara/Downloads/*.pdf'):
         text = pdf_to_str(path)
 
         tokens = tokenize(text)
         filtered_tokens = remove_stop_words(tokens)
         stemmed_filtered_tokens = stemming(filtered_tokens)
-        #term_frequency(stemmed_filtered_tokens, file_name=path.split('/')[-1])
+
+        # visualize the texts
+        term_frequency(stemmed_filtered_tokens, file_name=path.split('/')[-1])
         word_cloud(stemmed_filtered_tokens, file_name=path.split('/')[-1])
