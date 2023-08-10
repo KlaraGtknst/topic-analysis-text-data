e9c758daf5b6e75bc7dd93000c77c520e11f9bbf
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-08-10 12:05:02 +0200

    fix: improved slurm tasks

--- run_visualizations.sh
+++ run_visualizations.sh
@@ -1,5 +1,6 @@
 #!/bin/bash      
 #SBATCH --partition=main        # Partition main/ jupyter
+#SBATCH --job-name=vis-text      # Job-Name
 #SBATCH --nodes=1       # 1 Node wird benötigt
 #SBATCH --tasks-per-node=1      # Anzahl Tasks/ CPUs
 #SBATCH --mem=100       # 100 MB Hauptspeicher
@@ -8,6 +9,5 @@
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
 # (%N: Nodename, %j: Job-Nr.)
 source /mnt/stud/work/kgutekunst/bsc-py/bin/activate    # virtuelle Umgebung aktivieren
-python3 visualize_texts.py      # Programm ausfuehren
-ls
+srun /mnt/stud/work/kgutekunst/topic-analysis-text-data/visualize_texts.py -i '/mnt/datasets/Bahamas/SAC/0/SAC2-12.pdf' -o '/mnt/stud/home/kgutekunst/visualizations/'    # Programm ausfuehren
 echo "Hello from `hostname`"    # Programm ausfuehren
