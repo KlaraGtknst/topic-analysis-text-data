0de4c4d845bcbfc2058e704718f3df802c7900c0
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-08-10 11:49:57 +0200

    fix: fixed file (path) errors

--- run_visualizations.sh
+++ run_visualizations.sh
@@ -1,12 +1,13 @@
-#!/bin/bash	 
-#SBATCH --partition=main	# Partition main/ jupyter
-#SBATCH --nodes=1	# 1 Node wird benötigt
-#SBATCH --tasks-per-node=1	# Anzahl Tasks/ CPUs
-#SBATCH --mem=100	# 100 MB Hauptspeicher
-#SBATCH --time=0:05:00	# max. Laufzeit 5 min
-#SBATCH --output=/mnt/stud/home/kgutekunst/visualizations/%j.out	# Datei für stdout
-#SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err	# Datei für stderr
+#!/bin/bash      
+#SBATCH --partition=main        # Partition main/ jupyter
+#SBATCH --nodes=1       # 1 Node wird benötigt
+#SBATCH --tasks-per-node=1      # Anzahl Tasks/ CPUs
+#SBATCH --mem=100       # 100 MB Hauptspeicher
+#SBATCH --time=0:05:00  # max. Laufzeit 5 min
+#SBATCH --output=/mnt/stud/home/kgutekunst/visualizations/%j.out        # Datei für stdout
+#SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
 # (%N: Nodename, %j: Job-Nr.)
-source bsc-py/bin/activate    # virtuelle Umgebung aktivieren
-python3 visualizations.py	# Programm ausfuehren
-echo "Hello from `hostname`"	# Programm ausfuehren
+source /mnt/stud/work/kgutekunst/bsc-py/bin/activate    # virtuelle Umgebung aktivieren
+python3 visualize_texts.py      # Programm ausfuehren
+ls
+echo "Hello from `hostname`"    # Programm ausfuehren
