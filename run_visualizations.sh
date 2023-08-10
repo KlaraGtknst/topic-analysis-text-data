7c95825ee791a16e20ac012c82bc25897b02366f
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-08-10 12:43:11 +0200

    fix: slurm memory allocation unit

--- run_visualizations.sh
+++ run_visualizations.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=vis-text      # Job-Name
 #SBATCH --nodes=1       # 1 Node wird benötigt
 #SBATCH --tasks-per-node=1      # Anzahl Tasks/ CPUs
-#SBATCH --mem=100       # 100 MB Hauptspeicher
+#SBATCH --mem=100gb       # 100 MB Hauptspeicher
 #SBATCH --time=0:05:00  # max. Laufzeit 5 min
 #SBATCH --output=/mnt/stud/home/kgutekunst/visualizations/%j.out        # Datei für stdout
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
