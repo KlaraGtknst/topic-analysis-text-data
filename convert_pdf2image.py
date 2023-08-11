377eb45f2c21a6a57d2fac505d2c2d0fa680791b
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-08-11 08:50:37 +0200

    feat: added argparse output dir & default option

--- convert_pdf2image.py
+++ convert_pdf2image.py
@@ -8,13 +8,27 @@ run this code by typing and altering the path:
     python3 code_file.py -d '/Users/klara/Downloads/*.pdf'
 '''
 
+def pdf_to_png(file_path: str, outpath: str = None) -> None:
+    '''
+    :param file_path: path to file
+    :param outpath: path to output folder; if not set, the output folder is the same as the input folder.
+    :return: None
+
+    This function converts a PDF file to a PNG file.
+    The name of the PNG file is the same as the PDF file with .png instead of .pdf.
+    '''
+    for path in file_paths:
+        file_name = path.split('.')[0].split('/')[-1]
+        outpath = outpath if outpath else '/'.join(path.split('/')[:-1])
+        pages = convert_from_path(pdf_path=path, dpi=75, output_folder=outpath, output_file=file_name, fmt='png')
+
 if __name__ == '__main__':
     args = arguments()
     file_paths = get_input_filepath(args)
+    outpath = get_output_filepath(args)
 
-    for path in file_paths:
-        pages = convert_from_path(path, dpi=75, output_folder='/'.join(path.split('/')[:-1]), output_file=path.split('.')[0], fmt='png')
+    pdf_to_png(file_paths, outpath=outpath)
 
-        # TODO: 100 pdf a 64x64 pixel/dpi, in 10x10 matrix -> erkennen -> CNN Ähnlichkeiten erkennen
-        # erste seite reivht
+    # TODO: 100 pdf a 64x64 pixel/dpi, in 10x10 matrix -> erkennen -> CNN Ähnlichkeiten erkennen
+    # erste seite reivht
 
