2fc67740ea0bf87fcf0fa2c77aaeb0842bb39eb7
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-08-23 16:20:46 +0200

    chore: store logs in directory

--- sbatch/run_visualizations.sh
+++ sbatch/run_visualizations.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=100gb       # 100 GB Hauptspeicher
 #SBATCH --time=16:00:00  # max. Laufzeit 16h
-#SBATCH --output=/mnt/stud/home/kgutekunst/visualizations/%j.out        # Datei für stdout
+#SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out        # Datei für stdout (logs/ prints != results, e.g., .pdf files) 
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
 # (%N: Nodename, %j: Job-Nr.)
 
