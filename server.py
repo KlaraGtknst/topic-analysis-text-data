6a0af52903bd0c9f790625567682a0d811a28420
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-10 08:45:13 +0200

    feat: query for most similar embeddings in db

--- server.py
+++ server.py
@@ -18,9 +18,9 @@ def login():
     return 'login'
 
 # return 10 most similar documents to text x
-@app.route('/documents/<text>')
+@app.route('/documents/text/<text>')
 def find_most_likely_docs(text:str):
-    # http://127.0.0.1:8000/documents/bahamas
+    # http://127.0.0.1:8000/documents/text/bahamas
     elastic_search_client = Elasticsearch("http://localhost:9200")
     return query_database.text_search_db(elastic_search_client, text=text)
 
@@ -53,6 +53,40 @@ def hello(id):
     # TODO: save the file in the POST request, and then from the socket event read the file that you saved, cf. https://github.com/miguelgrinberg/Flask-SocketIO/issues/1351
     with open(resp_path, 'rb') as static_file:
         return send_file(static_file, download_name=f'{id}.pdf')
+    
+# return query for one document with query type x
+@app.route('/documents/<id>/query/<query_type>')
+def get_query(id, query_type): #TODO: async ?
+    # http://127.0.0.1:8000/documents/SAC1-6/query/TF-IDF
+    # get path of doc to search for
+    elastic_search_client = Elasticsearch("http://localhost:9200")
+    doc_to_search_for = query_database.get_doc_meta_data(elastic_search_client, doc_id=id)['path']
+
+    # start query
+    if query_type == 'TF-IDF':
+        # alter src_path & client address if necessary
+        query_results = query_database.get_sim_docs_tfidf(doc_to_search_for, src_paths=None)
+        
+    elif query_type == 'cluster':
+        query_results = query_database.get_docs_from_same_cluster(elastic_search_client = elastic_search_client, path_to_doc = doc_to_search_for, n_results=10)#NUM_RESULTS)
+
+    elif query_type == 'Doc2Vec':
+        query_results = query_database.search_sim_doc2vec_docs_in_db(path=doc_to_search_for, client=elastic_search_client, doc2vec_model=None, src_paths=None)#SRC_PATH)
+
+    elif query_type == 'UniversalSentenceEncoder':
+       # TODO: await?
+       query_results = query_database.find_sim_docs_google_univSentEnc(path=doc_to_search_for, client=elastic_search_client)
+       #print(query_results.keys())
+
+    elif query_type == 'HuggingFaceSentenceTransformer':
+        query_results = query_database.find_sim_docs_hugging_face_sentTrans(path=doc_to_search_for, client=elastic_search_client)
+
+    elif query_type == 'InferSent':
+        query_results = query_database.find_sim_docs_inferSent(path=doc_to_search_for, client=elastic_search_client, src_paths=None)# glob.glob(SRC_PATH))
+    
+    result = [hit['_source'] for hit in query_results['hits']['hits']]
+    return result
+
 
 # return wordcloud of one document as PNG
 @app.route('/documents/<id>/wordcloud')
