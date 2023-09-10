177cc34d6d5089cb70819176c892aa25b7879877
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-10 14:14:52 +0200

    feat: added files for constants

---
+++ constants.py
@@ -0,0 +1,2 @@
+EMBEDDINGS = ["doc2vec", "sim_docs_tfidf", "google_univ_sent_encoding", "huggingface_sent_transformer", "inferSent_AE"]
+DB_FIELDS = EMBEDDINGS + ["pca_kmeans_cluster", "path", "text", "image"]
