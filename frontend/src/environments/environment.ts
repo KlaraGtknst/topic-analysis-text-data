b01c7f002ee07b78c75342018bb2ddb115df9c06
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-12 23:30:00 +0200

    fix: changed routes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,4 @@
 export const environment = {
   production: false,
-  backendBaseUrl: "http://localhost:3000",
-  backendWSUrl: "http://localhost:3000",
-  keycloakUrl: "http://localhost:8080",
-  keycloakRealm: "fski",
-  keycloakClientId: "meldekopf-frontend",
-
-  fahnderRightTemplate: { canRead: true, canWrite: false, role: "FahnderIn" },
-  itRightTemplate: { canRead: true, canWrite: false, role: "IT" },
-  objektleitungRightTemplate: { canRead: true, canWrite: true, role: "Objektleitung" },
-  meldekopfRightTemplate: { canRead: true, canWrite: true, role: "Meldekopf" },
-  federfuehrungRightTemplate: { canRead: true, canWrite: true, role: "Federfuerung" },
+  baseurl: 'http://127.0.0.1:8000/'
 };
