a4a52a1785449c826ede7ba90f3865562aadd1fc
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-13 20:35:23 +0200

    feat: found option to recursively find all PDFs

---
+++ test_functions/test_scanning_file_tree.py
@@ -0,0 +1,23 @@
+# %%
+import glob
+import os
+base_path = '/Users/klara/Documents/uni/bachelorarbeit'
+print(base_path)
+# %%
+paths = [path for path in os.scandir(base_path) if path.name.endswith('.pdf')]
+print(paths)
+# %%
+for fp in paths:
+    print(fp.path)
+# %%
+paths2 = [path for path in glob.glob(base_path + '/') if path.endswith('.pdf')]
+print(paths2)
+
+# %%
+print(os.listdir(base_path))
+# %%
+# best option
+all_paths = [os.path.join(r,file) for r,d,f in os.walk(base_path) for file in f if file.endswith('.pdf')]
+print(all_paths)
+print(len(all_paths))
+# %%
