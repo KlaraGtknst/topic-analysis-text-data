8e863cbe394ca48138f473378dce93ec0e01fc05
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-14 13:11:10 +0200

    chore: requested more memory

--- sbatch/run_pdf2png.sh
+++ sbatch/run_pdf2png.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=pdf2png-bahamas      # Job-Name
 #SBATCH --nodes=1       # 1 Node wird benötigt
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=100gb       # 100 GB Hauptspeicher
+#SBATCH --mem=256gb       # 100 GB Hauptspeicher
 #SBATCH --time=16:00:00  # max. Laufzeit 16h
 #SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out        # Datei für stdout (logs/ prints != results, e.g., .pdf files) 
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
