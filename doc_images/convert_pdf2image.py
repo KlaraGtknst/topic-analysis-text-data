9db4d5d35236ded704dcfb32672eef243bf21189
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-15 13:14:30 +0200

    fix: reduce memory usage of pdf2png

--- doc_images/convert_pdf2image.py
+++ doc_images/convert_pdf2image.py
@@ -21,8 +21,8 @@ def pdf_to_png(file_path: list, outpath: str = None, save: bool= True) -> list:
     https://stackoverflow.com/questions/69643954/converting-pdf-to-png-with-python-without-pdf2image
     https://pymupdf.readthedocs.io/en/latest/pixmap.html#Pixmap.set_dpi
     '''
-    broken_files = []
-    images = []
+    #broken_files = []
+    #images = []
     for path in file_path:
         file_name = (path.split('.')[0]).split('/')[-1]
         outpath = outpath if outpath else '/'.join(path.split('/')[:-1])
@@ -30,16 +30,17 @@ def pdf_to_png(file_path: list, outpath: str = None, save: bool= True) -> list:
         try:
             pix = doc[0].get_pixmap()  # render first page to an image
             pix.set_dpi(75, 75) # image resolution
-            images.append(pix)
+            #images.append(pix)
             if save:
                 pix.save(f"{outpath}/{file_name}.png")
+            doc.close()
         except:
             # print(f'file {path} could not be converted to png')
-            broken_files.append(path)
+           ''' broken_files.append(path)
 
     if broken_files:
-        print(f'files {broken_files} could not be converted to png')
-    return images
+        print(f'files {broken_files} could not be converted to png')'''
+    #return images
 
 def main(file_paths, outpath):
 
