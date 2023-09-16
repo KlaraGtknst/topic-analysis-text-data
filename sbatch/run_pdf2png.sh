db86cbece0fb978d58b2b57192c07d3fb22da76c
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-16 09:16:01 +0200

    fix: more time to run script

--- sbatch/run_pdf2png.sh
+++ sbatch/run_pdf2png.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1       # 1 Node wird benötigt
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=256gb       # 100 GB Hauptspeicher
-#SBATCH --time=48:00:00  # max. Laufzeit 48h
+#SBATCH --time=56:00:00  # max. Laufzeit 56h
 #SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out        # Datei für stdout (logs/ prints != results, e.g., .pdf files) 
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
 # (%N: Nodename, %j: Job-Nr.)
