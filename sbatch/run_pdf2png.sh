1360c0b314d19ead65b2773515282f63f0b7a3fa
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-09-16 09:15:33 +0200

    fix: more time to run script

--- sbatch/run_pdf2png.sh
+++ sbatch/run_pdf2png.sh
@@ -4,13 +4,13 @@
 #SBATCH --nodes=1       # 1 Node wird benötigt
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=256gb       # 100 GB Hauptspeicher
-#SBATCH --time=16:00:00  # max. Laufzeit 16h
+#SBATCH --time=48:00:00  # max. Laufzeit 48h
 #SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out        # Datei für stdout (logs/ prints != results, e.g., .pdf files) 
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
 # (%N: Nodename, %j: Job-Nr.)
 
 date;hostname;pwd    # Ausgabe des Datums, des Hostnamens und des Arbeitsverzeichnisses
 source /mnt/stud/work/kgutekunst/bsc-py/bin/activate    # virtuelle Umgebung aktivieren
-srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/main_server.py 'convert_pdf2image.py' -d '/mnt/datasets/Bahamas' -o '/mnt/stud/home/kgutekunst/visualizations/pdf2png/'  # Programm ausfuehren
+srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/main_server.py 'convert_pdf2image.py' -d '/mnt/datasets/Bahamas' -o '/mnt/stud/home/kgutekunst/visualizations/images/'  # Programm ausfuehren
 #srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/main.py 'convert_pdf2image.py' -d '/mnt/datasets/Bahamas' -o '/mnt/stud/home/kgutekunst/visualizations/images/'  # Programm ausfuehren
 #srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/doc_images/convert_pdf2image.py -d '/mnt/datasets/Bahamas' -o '/mnt/stud/home/kgutekunst/visualizations/images/'  # Programm ausfuehren
