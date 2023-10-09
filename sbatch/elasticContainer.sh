7febbe76573f094e0c1fd6f4a09fb812882501a4
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-10-09 09:38:31 +0200

    chore: different node sbatch

--- sbatch/elasticContainer.sh
+++ sbatch/elasticContainer.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=elastic-db  
 #SBATCH --nodes=1       
 #SBATCH --cpus-per-task=8
-#SBATCH --nodelist=cpu-epyc-5
-#SBATCH --mem=1g       
+#SBATCH --nodelist=cpu-epyc-6
+#SBATCH --mem=64g       
 #SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out    
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err   
 
