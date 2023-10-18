96ad048ee05691257f4cba9c58ebd919f639be2c
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-10-18 22:50:54 +0200

    fix: try more cpus

--- sbatch/create_documents.sh
+++ sbatch/create_documents.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=main        # Partition main
 #SBATCH --job-name=create-docs  # Job-Name
 #SBATCH --nodes=1       # 1 Node wird benötigt
-#SBATCH --cpus-per-task=45 
+#SBATCH --cpus-per-task=128 
 #SBATCH --nodelist=cpu-epyc-7
-#SBATCH --mem=200g       # 264 GB Hauptspeicher
+#SBATCH --mem=400g
 #SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out        # Datei für stdout (logs/ prints != results, e.g., .pdf files) 
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
 # (%N: Nodename, %j: Job-Nr.)
@@ -12,4 +12,4 @@
 date;hostname;pwd    # Ausgabe des Datums, des Hostnamens und des Arbeitsverzeichnisses
 source /mnt/stud/work/kgutekunst/bsc-py/bin/activate    # virtuelle Umgebung aktivieren
 
-srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/main_server.py 'create_documents.py' -p 35 -a 'http://cpu-epyc-7.ies.uni-kassel.de:9200' -d '/mnt/datasets/Bahamas/*.pdf' -D '/mnt/stud/home/kgutekunst/visualizations/images/'
+srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/main_server.py 'create_documents.py' -p 128 -a 'http://cpu-epyc-7.ies.uni-kassel.de:9200' -d '/mnt/datasets/Bahamas/*.pdf' -D '/mnt/stud/home/kgutekunst/visualizations/images/'
