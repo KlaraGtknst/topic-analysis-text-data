1f3a63bc2c0f06983d4c8e7996b86b05fd9b0ed5
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-10-22 10:11:48 +0200

    feat: select multiple docs per dir

--- elasticSearch/selected_docs.py
+++ elasticSearch/selected_docs.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-def select_rep_path(baseDir: str):
+def select_rep_path(baseDir: str, num_items_per_dir:int=1):
     print(baseDir)
     baseDir = baseDir.split('*')[0] if  '*' in baseDir else baseDir
     baseDir = baseDir if baseDir.endswith('/') else baseDir + '/'
@@ -9,11 +9,12 @@ def select_rep_path(baseDir: str):
     selected_docs = []
     for root, dirs, files in os.walk(baseDir):
         for dir in dirs:
-            file = random.choice(os.listdir(os.path.join(root, dir)))
-            file_path = os.path.join(root, dir, file)
+            files = random.choices(os.listdir(os.path.join(root, dir)), k=num_items_per_dir)
+            for file in files:
+                file_path = os.path.join(root, dir, file)
 
-            if (os.path.isfile(file_path) and (not file.startswith('.'))):
-                selected_docs.append(file_path)
+                if (os.path.isfile(file_path) and (not file.startswith('.'))):
+                    selected_docs.append(file_path)
 
     return selected_docs
         
@@ -21,4 +22,4 @@ def select_rep_path(baseDir: str):
 
 if __name__ == '__main__':
     baseDir = '/Users/klara/Documents/uni/'
-    print(select_rep_path(baseDir))
+    print(select_rep_path(baseDir, 2))
