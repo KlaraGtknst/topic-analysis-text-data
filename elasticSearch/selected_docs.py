7becaa7ccd1d67360c8990ac4215693d00f4b976
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-10-22 14:07:07 +0200

    fix: sample from existing files

--- elasticSearch/selected_docs.py
+++ elasticSearch/selected_docs.py
@@ -8,12 +8,14 @@ def select_rep_path(baseDir: str, num_items_per_dir:int=1):
     selected_docs = []
     for root, dirs, files in os.walk(baseDir):
         for dir in dirs:
-            files = random.choices(os.listdir(os.path.join(root, dir)), k=num_items_per_dir)
-            for file in files:
-                file_path = os.path.join(root, dir, file)
+            paths = os.listdir(os.path.join(root, dir))
+            if len(paths) > 0:
+                files = random.choices(paths, k=min(num_items_per_dir, len(paths)))
+                for file in files:
+                    file_path = os.path.join(root, dir, file)
 
-                if (os.path.isfile(file_path) and (not file.startswith('.'))):
-                    selected_docs.append(file_path)
+                    if (os.path.isfile(file_path) and (not file.startswith('.'))):
+                        selected_docs.append(file_path)
 
     return selected_docs
         
