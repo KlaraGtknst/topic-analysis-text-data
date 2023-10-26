1a988857fd362ece35ee581a842eb80df54a5363
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-10-26 19:13:40 +0200

    feat: test pool

---
+++ elasticSearch/test_pool.py
@@ -0,0 +1,56 @@
+from multiprocessing import Pool
+import os
+import sys
+
+def insert_embeddings(x,y,z,d):
+    print(len(x),y,z,d)
+
+class wrapper:
+    def __init__(self, model_name):
+        self.model_name = model_name
+
+    def __call__(self, src_paths):
+        insert_embeddings(src_paths, "fixed 1", "fixed 2", self.model_name)
+
+def scanRecurse(baseDir: str):
+    baseDir = baseDir.split('*')[0] if  '*' in baseDir else baseDir
+
+    for entry in os.scandir(baseDir):
+        if entry.is_file():
+            yield os.path.join(baseDir, entry.name)
+        else:   # recurse needs from, otherwise generator object is returned
+            yield from scanRecurse(entry.path + '/')
+
+
+def chunks(lst:list, n:int):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def temp(x):
+    print('Process ', len(x))
+
+
+MODEL_NAMES = ['doc2vec', 'universal', 'hugging', 'infer', 'ae', 'tfidf']
+
+def main(src_path: str, model_names: list = MODEL_NAMES, num_cpus:int=1):
+    print('start inserting documents embeddings')
+
+    # all paths
+    document_paths = list(scanRecurse(src_path))
+    sub_lists = list(chunks(document_paths, int(len(document_paths)/num_cpus)))
+
+    # process n_cpus sublists
+    with Pool(processes=num_cpus) as pool:
+        for model_name in model_names:  # function und diese parallisieren: run_process(doc_paths)
+            proc_wrap = wrapper(model_name)
+            print('started with model: ', model_name)
+            pool.map(proc_wrap, sub_lists)
+            print('finished model: ', model_name)
+
+    print('finished inserting documents embeddings')
+
+
+if __name__=='__main__':
+    # python elasticSearch/test_pool.py /Users/klara/Documents/Uni/bachelorarbeit/data/0/ 2
+    main(sys.argv[1], num_cpus=int(sys.argv[2]))
--- main_server.py
+++ main_server.py
@@ -1,7 +1,7 @@
 from constants import CLIENT_ADDR
 from user_interface.cli import *
 from doc_images import convert_pdf2image
-from elasticSearch import db_elasticsearch, create_documents, create_database, insert_embeddings, insert_clusters, selected_docs
+from elasticSearch import db_elasticsearch, create_documents, create_database, insert_embeddings, insert_clusters, selected_docs, test_pool
 from doc_images.PCA import PCA_image_clustering
 from text_embeddings.InferSent import own_word2vec
 from text_embeddings.autoencoder import opt_architecture_ae
@@ -72,5 +72,11 @@ if __name__ == '__main__':
     elif file_to_run[0] == 'selected_docs.py':
         # python3 main_server.py 'selected_docs.py' -d '/Users/klara/Documents/Uni/*.pdf' -D '/Users/klara/Documents/Uni/bachelorarbeit/images/' -p 1
         selected_docs.main(file_path)
+
+    elif file_to_run[0] == 'test_pool.py':
+        # python3 main_server.py 'test_pool.py' -d '/Users/klara/Documents/Uni/bachelorarbeit/data/0/*.pdf' -D '/Users/klara/Documents/Uni/bachelorarbeit/images/' -p 1
+        test_pool.main(src_path=file_path, model_names=model_names, num_cpus=n_pools)
+
+
 
         
