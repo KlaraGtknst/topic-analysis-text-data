e9f1e6dac9e64651bb49af1f5ecea3f8e8c23fe1
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-10-31 22:55:09 +0100

    style: code clean up

--- elasticSearch/insert_embeddings.py
+++ elasticSearch/insert_embeddings.py
@@ -33,14 +33,14 @@ class wrapper:
 
 def get_src_paths(src_path: str, src_paths: list):
     if src_paths == []:
-        return scanRecurse(src_path)    # bulk, local
+        return scanRecurse(src_path)    # local
     else:
-        return src_paths    # parallel, server
+        return src_paths    # parallel using Pool server
 
 def generate_models_embedding(src_path: str, src_paths: list, models : dict, client: Elasticsearch, model_name: str = 'no_model'):  
-    print('started with generate_models_embedding() ')
-    #sys.stdout.flush()
-    print('model_name: ', model_name, ' models.keys(): ', models.keys())
+    print('started with generate_models_embedding(), model_name: ', model_name)
+    sys.stdout.flush()
+   
     for path in get_src_paths(src_path=src_path, src_paths=src_paths):
     
         text = pdf_to_str(path)
@@ -66,7 +66,8 @@ def insert_embedding(src_path: str, src_paths: list, models: dict={}, client_add
         inserts specific embedding of all documents into the database 'bahamas'.
         '''
         print('started with insert_embedding() ')
-        #sys.stdout.flush()
+        sys.stdout.flush()
+
         if (model_name not in MODEL_NAMES) or (model_name == 'ae') or (model_name == 'none'):
             return
         
@@ -87,7 +88,6 @@ def get_embedding(models: dict, model_name: str, text: str):
     :param text: text to be embedded
     :return: embedding of the text
     '''
-    #print('started with get_embedding() ')
     if model_name == "doc2vec": 
         return models['doc2vec'].infer_vector(simple_preprocess(text))
     
@@ -97,20 +97,17 @@ def get_embedding(models: dict, model_name: str, text: str):
     elif model_name in ['huggingface_sent_transformer', 'hugging']:
         if type(text) == str:
             text = [text]
-
         return models['hugging'].encode(sentences=text)[0]
     
     elif model_name in ['inferSent_AE', 'infer']:
-        print('init start infer emb ', len(text), [text], type(text))
         inferSent_embedding = models['infer'].encode([text], tokenize=True)
-    
         return models['ae'].predict(x=inferSent_embedding)[0]
                     
     elif model_name in ['sim_docs_tfidf', 'tfidf']:
-        print('entered tfidf    ', [text])
         tfidf_embedding = get_tfidf_emb(models['tfidf'], [text])
         print('tfidf_embedding before ae: ', tfidf_embedding, tfidf_embedding.shape)
-        if len(tfidf_embedding) > 2048:
+
+        if len(tfidf_embedding) > 2048: # AE to compress data
             tfidf_ae_model = models['tfidf_ae']
             tfidf_embedding = tfidf_embedding.reshape(1, tfidf_embedding.shape[0])
             print('tfidf_embedding after ae: ', tfidf_embedding, tfidf_embedding.shape)
@@ -118,138 +115,30 @@ def get_embedding(models: dict, model_name: str, text: str):
         return tfidf_embedding
 
 
-# PCA & OPTICS
-
-def pca_optics_aux(pca_dict: dict):  
-    for id in pca_dict.index:
-        yield {
-            '_op_type': 'update',
-            '_index': 'bahamas',
-            '_id': id,
-            'doc': {
-                "pca_optics_cluster": pca_dict['cluster'][id],
-                }
-        }
-
-def insert_pca_optics(pca_dict: dict, client_addr=CLIENT_ADDR, client: Elasticsearch=None):
-        '''
-        :param client: Elasticsearch client
-        :param pca_dict: dictionary with weights and clusters
-
-        inserts pca weights and OPTICS cluster of all documents into the database 'bahamas'.
-        '''
-        client = client if client else Elasticsearch(client_addr, timeout=1000)
-      
-        try:
-            bulk(client, pca_optics_aux(pca_dict), stats_only= True)
-        except (ConflictError, ApiError,EOFError) as err:
-            print('error')
-            return
-        
-def pca_weights_aux(src_path: str, image_root_path:str, max_w:int, max_h:int, pca_model: decomposition.PCA): 
-    '''
-    :param src_path: path to the directory of the documents to be inserted into the database
-    :param image_root_path: path to the images
-    :param max_w: max width of the images
-    :param max_h: max height of the images
-    :param pca_model: fitted PCA model
-
-    inserts pca weights and argmax clusters of all documents into db
-    ''' 
-    for path in scanRecurse(src_path):
-      
-        id = get_hash_file(path)
-        img_path = image_root_path + path.split('/')[-1].split('.')[0] + '.png'
-        if not os.path.exists(img_path):
-            print('not existent: ', path)
-            pdf_to_png(file_path= [path], outpath=image_root_path, save= True)
-        if not os.path.exists(img_path):    # some pdf cannot be read
-            document = np.asarray([np.zeros((max_w, max_h)).ravel()])
-        else:
-            document_raw = plt.imread(img_path)
-            new_w = np.minimum(document_raw.shape[0], max_w)
-            new_h = np.minimum(document_raw.shape[1], max_h)
-            document_raw = np.resize(document_raw, (new_w, new_h))
-            
-            document = eigendocs.proprocess_docs(raw_documents=[document_raw], max_w=max_w, max_h=max_h)
-
-        reduced_img = pca_model.transform(document)
-       
-        yield {
-            '_op_type': 'update',
-            '_index': 'bahamas',
-            '_id': id,
-            'doc': {
-                "pca_image": reduced_img[0],
-                "argmax_pca_cluster": np.argmax(reduced_img),
-                }
-        }
-        
-def insert_pca_weights(src_path: str, pca_model: decomposition.PCA, img_path:str, max_w:int, max_h:int, client_addr=CLIENT_ADDR, client: Elasticsearch=None):
-        '''
-        :param src_path: paths to the directory of the documents to be inserted into the database
-        :param client: Elasticsearch client
-        :param pca_model: fitted PCA model
-
-        inserts pca weights and OPTICS cluster of all documents into the database 'bahamas'.
-        '''
-        client = client if client else Elasticsearch(client_addr, timeout=1000)
-      
-        try:
-            bulk(client, pca_weights_aux(src_path=src_path, pca_model=pca_model, image_root_path=img_path, max_w=max_w, max_h=max_h), stats_only= True)
-        except (ConflictError, ApiError,EOFError) as err:
-            print('error')
-            return
-
-def insert_precomputed_clusters(src_path: str, image_src_path:str, client_addr:str=CLIENT_ADDR):
-    # get PCA model
-    print('start getting pca model')
-    pca_model, max_w, max_h = get_eigendocs_PCA(img_dir_src_path=image_src_path, n_components=NUM_PCA_COMPONENTS)
-    print('finished getting pca model')
-
-    # save weights and argmax cluster in db
-    insert_pca_weights(src_path=src_path, pca_model=pca_model, img_path=image_src_path, client_addr=client_addr, max_w=max_w, max_h=max_h)
-    print('finished inserting pca-argmax cluster df')
-
-    # OPTICS clusters
-    # get all pca weights and id 
-    elastic_search_client = Elasticsearch(client_addr, timeout=1000)
-    results = get_all_docs_in_db(elastic_search_client, src_includes = ['pca_image'])   # _id, pca_image
-    result_df = pd.DataFrame.from_dict(results)
-    result_df.set_index('_id', inplace=True)
-    
-    # clustering
-    clt = OPTICS(cluster_method='dbscan', min_samples=2, max_eps=10, eps=0.5)
-    result_df['cluster'] = clt.fit_predict(np.array(result_df['pca_image'].values.tolist()))
-
-    print('finished getting pca-OPTICS cluster df')
-    insert_pca_optics(pca_dict=result_df, client_addr=client_addr)
-    print('finished inserting pca-OPTICS cluster df')
-
 def main(src_path: str, client_addr=CLIENT_ADDR, model_names: list = MODEL_NAMES, num_cpus:int=1):
-    src_path = '/Users/klara/Documents/uni/bachelorarbeit/data/*.pdf'
     print('start inserting documents embeddings')
 
-    if num_cpus == 1:
-        for model_name in model_names:  # function und diese parallisieren: run_process(doc_paths)
+    if num_cpus == 1:   # FIXME: Pool does not work locally, even if num_cpus = 1
+        for model_name in model_names:
             print('started with model: ', model_name)
             insert_embedding(src_path = src_path, src_paths=[], client_addr=client_addr, model_name = model_name)
             print('finished model: ', model_name)
-    else:
-        # all paths
+
+    else:   # server uses parallelization (Pool)
         document_paths = list(scanRecurse(src_path))
         print('number of docs: ', len(document_paths))
         sub_lists = list(chunks(document_paths, int(len(document_paths)/num_cpus)))
-
         print('obtained sublists')
 
         # process n_cpus sublists
         with Pool(processes=num_cpus) as pool:
             for model_name in model_names:  # function und diese parallisieren: run_process(doc_paths)
+                print('started with model: ', model_name)
+
                 proc_wrap = wrapper(model_name=model_name, baseDir=src_path)
                 print('initialized wrapper')
-                print('started with model: ', model_name)
                 sys.stdout.flush()
+
                 pool.map(proc_wrap, sub_lists)
                 print('finished model: ', model_name)
 
