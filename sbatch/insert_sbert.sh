12400adc6a34c7c9b191cf0d0d520c5c853ff3ff
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-11-02 07:21:41 +0100

    feat: try less nodes sbatch SBERT

--- sbatch/insert_sbert.sh
+++ sbatch/insert_sbert.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=main        # Partition main
 #SBATCH --job-name=sbert-hf  # Job-Name
 #SBATCH --nodes=1      
-#SBATCH --cpus-per-task=128
+#SBATCH --cpus-per-task=36
 #SBATCH --nodelist=cpu-epyc-7
 #SBATCH --mem=200g
 #SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out        
@@ -12,4 +12,4 @@
 date;hostname;pwd 
 source /mnt/stud/work/kgutekunst/bsc-py/bin/activate  
 
-srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/main_server.py 'insert_embeddings.py' -m 'hugging' -p 128 -a 'http://cpu-epyc-7.ies.uni-kassel.de:9200' -d '/mnt/datasets/Bahamas/*.pdf' -D '/mnt/stud/home/kgutekunst/visualizations/images/'
+srun python /mnt/stud/work/kgutekunst/topic-analysis-text-data/main_server.py 'insert_embeddings.py' -m 'hugging' -p 36 -a 'http://cpu-epyc-7.ies.uni-kassel.de:9200' -d '/mnt/datasets/Bahamas/*.pdf' -D '/mnt/stud/home/kgutekunst/visualizations/images/'
