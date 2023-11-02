28843936806b7dc848df5fb87d6a05fdf5462997
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-11-02 07:21:14 +0100

    feat: timing of embeddings

--- evaluation/embs_timing.py
+++ evaluation/embs_timing.py
@@ -36,6 +36,32 @@ def generate_models_embedding(src_path: str, models : dict, model_name: str = 'n
                 continue
         
 
+def get_doc2vec_embedding(models, text):
+    return models['doc2vec'].infer_vector(simple_preprocess(text))
+
+def get_USE_embedding(models, text):
+    return embed([text], models['universal'])[0].numpy()
+
+def get_SBERT_embedding(models, text):
+    if type(text) == str:
+            text = [text]
+    return models['hugging'].encode(sentences=text)[0]
+
+
+def get_InferSent_embedding(models, text):
+    inferSent_embedding = models['infer'].encode([text], tokenize=True)
+    return models['ae'].predict(x=inferSent_embedding)[0]
+
+
+def get_TFIDF_embedding(models, text):
+    tfidf_embedding = get_tfidf_emb(models['tfidf'], [text])
+
+    if len(tfidf_embedding) > 2048: # AE to compress data
+        tfidf_ae_model = models['tfidf_ae']
+        tfidf_embedding = tfidf_embedding.reshape(1, tfidf_embedding.shape[0])
+        return tfidf_ae_model.predict(x=tfidf_embedding)[0]
+    return tfidf_embedding
+
 
 def get_embedding(models: dict, model_name: str, text: str):
     '''
@@ -45,30 +71,41 @@ def get_embedding(models: dict, model_name: str, text: str):
     :return: embedding of the text
     '''
     if model_name == "doc2vec": 
-        return models['doc2vec'].infer_vector(simple_preprocess(text))
+        return get_doc2vec_embedding(models, text)
     
     elif model_name in ['google_univ_sent_encoding', 'universal']:
-        return embed([text], models['universal'])[0].numpy()
+        return get_USE_embedding(models, text)
     
     elif model_name in ['huggingface_sent_transformer', 'hugging']:
-        if type(text) == str:
-            text = [text]
-        return models['hugging'].encode(sentences=text)[0]
+        return get_SBERT_embedding(models, text)
     
     elif model_name in ['inferSent_AE', 'infer']:
-        inferSent_embedding = models['infer'].encode([text], tokenize=True)
-        return models['ae'].predict(x=inferSent_embedding)[0]
+        return get_InferSent_embedding(models, text)
                     
     elif model_name in ['sim_docs_tfidf', 'tfidf']:
-        tfidf_embedding = get_tfidf_emb(models['tfidf'], [text])
-        print('tfidf_embedding before ae: ', tfidf_embedding, tfidf_embedding.shape)
+        return get_TFIDF_embedding(models, text)
 
-        if len(tfidf_embedding) > 2048: # AE to compress data
-            tfidf_ae_model = models['tfidf_ae']
-            tfidf_embedding = tfidf_embedding.reshape(1, tfidf_embedding.shape[0])
-            print('tfidf_embedding after ae: ', tfidf_embedding, tfidf_embedding.shape)
-            return tfidf_ae_model.predict(x=tfidf_embedding)[0]
-        return tfidf_embedding
+
+def post_process_time_file(file_path: str= 'times_out.txt'):
+    # Opening file
+    file1 = open(file_path, 'r')
+    count = 0
+
+    method_names = ['get_doc2vec_embedding', 'get_USE_embedding', 'get_SBERT_embedding', 'get_InferSent_embedding', 'get_TFIDF_embedding']
+    
+    # Using for loop
+    for line in file1:
+        count += 1
+        #print(line)
+        print('Hi')
+        cum_time = line.strip().split(' ')[3]
+        function_name = line.strip().split(' ')[5:]
+        if function_name in method_names:
+            #print("Line{}:".format(count), cum_time, function_name)
+            print('2')
+    
+    # Closing files
+    file1.close()
 
 
 def main(baseDir: str, model_names: list = MODEL_NAMES):
@@ -77,3 +114,5 @@ def main(baseDir: str, model_names: list = MODEL_NAMES):
         models = get_models(baseDir, [model_name] if model_name else None)
         generate_models_embedding(src_path=baseDir, models=models, model_name=model_name)
         print('finished model: ', model_name)
+
+    # post_process_time_file()
