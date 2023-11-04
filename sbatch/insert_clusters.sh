9b44b20c066125642a4f746e230eeff7582f22d5
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-11-04 21:34:20 +0100

    feat: update sbatch clusters

--- sbatch/insert_clusters.sh
+++ sbatch/insert_clusters.sh
@@ -2,10 +2,9 @@
 #SBATCH --partition=main        # Partition main
 #SBATCH --job-name=clusters  # Job-Name
 #SBATCH --nodes=1       # 1 Node wird benötigt
-#SBATCH --cpus-per-task=45 
+#SBATCH --cpus-per-task=1
 #SBATCH --nodelist=cpu-epyc-7
 #SBATCH --mem=264g       # 264 GB Hauptspeicher
-#SBATCH --time=150:00:00  # max. Laufzeit 100h
 #SBATCH --output=/mnt/stud/home/kgutekunst/logs/%j.out        # Datei für stdout (logs/ prints != results, e.g., .pdf files) 
 #SBATCH --error=/mnt/stud/home/kgutekunst/error_logs/%j.err     # Datei für stderr
 # (%N: Nodename, %j: Job-Nr.)
