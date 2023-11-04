1315d704757d2a603c6c3eaef03b8a90d35f202b
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2023-11-04 16:58:58 +0100

    feat: visualize query results

--- evaluation/com_embs.py
+++ evaluation/com_embs.py
@@ -1,16 +1,19 @@
 
 
 import ast
+import os
 import random
 import re
+import cv2
 
 from elasticsearch import Elasticsearch
 import numpy as np
 import pandas as pd
 from constants import *
 from elasticSearch.create_documents import get_hash_file
-from elasticSearch.queries.query_database import get_knn_res
+from elasticSearch.queries.query_database import get_knn_res, get_doc_meta_data
 from elasticSearch.recursive_search import scanRecurse
+from doc_images.pdf_matrix import alter_axes
 import venn
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -37,10 +40,14 @@ def create_sim_docs_log(baseDir:str, client_addr:str=CLIENT_ADDR, num_rep_docs:i
     for id in ids:
         row = {}
         for model in model_names: 
-            res = get_knn_res(doc_to_search_for=id, elastic_search_client=client, query_type=MODELS2EMB[model], n_results=n_res)
+            res = get_knn_res(doc_to_search_for=id, elastic_search_client=client, query_type=MODELS2EMB[model], n_results=n_res + 1)
             if type(res)==dict:
                 continue
-            row[model] = [cell['_id'] for cell in res]
+            for cell in res:
+                if id == cell['_id']: # remove query document from results
+                    res.remove(cell)
+            res = res[:n_res]   # remove last element if more than n_res results, i.e. query id is not included
+            row[model] = [cell['_id'] for cell in res] 
         res_dict.loc[id] = row
     return res_dict
 
@@ -62,7 +69,7 @@ def read_from_dir(path_to_file:str):
     return pd.read_csv(filepath_or_buffer=path_to_file, sep=',', index_col='search_id')
 
 
-def visualize_using_venn(df:pd.DataFrame, save:bool=False, resDir:str='results/'):
+def visualize_using_venn(df:pd.DataFrame, save:bool=False, resDir:str='results/', title:str='Venn diagram of most similar documents'):
     '''
     :param df: df which stores most similar documents for each model for each query
     :param save: if True, venn diagram is saved to resDir
@@ -79,7 +86,6 @@ def visualize_using_venn(df:pd.DataFrame, save:bool=False, resDir:str='results/'
             dataset[model].update(set(cell))
     labels = venn.get_labels(dataset.values(), fill=['number'])
     fig, ax = venn.venn6(labels, names=dataset.keys())
-    title = 'Venn diagram of most similar documents'
     plt.title(title)
     if save:
         plt.savefig(resDir + re.sub(' ', '_', string=title) + '.pdf', format='pdf', bbox_inches = 'tight')
@@ -159,16 +165,80 @@ def create_sim_heatmap(res_df:pd.DataFrame, save:bool=False, resDir:str='results
         plt.savefig(resDir + re.sub(' ', '_', string=title) + '.pdf', format='pdf', bbox_inches = 'tight')
     plt.show()
 
+def get_img_path(doc_path:str, image_baseDir:str):
+    image_baseDir = image_baseDir if image_baseDir.endswith('/') else image_baseDir + '/'
+    file_name = doc_path.split('/')[-1].split('.')[0]
+    image_path = image_baseDir + file_name + '.png'
+    return image_path
+
+def add_border_around_subfig(ax):
+    ax.patch.set_linewidth(10)
+    ax.patch.set_edgecolor('pink')
+
+def create_query_img(query: pd.DataFrame, image_baseDir:str, outpath:str='', client_addr:str=CLIENT_ADDR):
+    '''
+    :param query: a df which contains the id as a column, and columns for the most similar documents per embedding model (column name)
+    '''
+    # get paths to documents
+    client = Elasticsearch(client_addr)
+    for doc_idx in range(len(query.index)):
+        meta_data = get_doc_meta_data(client, query.iloc[doc_idx].name)
+        path = meta_data['path']
+
+        # get path to query image
+        image_path = get_img_path(doc_path=path, image_baseDir=image_baseDir)
+
+        # get path to response document images
+        for model in get_model_names():
+            paths =[image_path]
+            for resp_doc in ast.literal_eval(query.iloc[doc_idx][model]):
+                doc_path = get_doc_meta_data(client, resp_doc)['path']
+                paths.append(get_img_path(doc_path=doc_path, image_baseDir=image_baseDir))
+
+            # display images
+            fig, axs = plt.subplots(nrows=1, ncols=len(paths), figsize=(len(paths)*5,5))
+            fig.subplots_adjust(hspace = .00, wspace= .00)
+            for i, img in enumerate(paths):
+                ax = fig.add_subplot(1, len(paths), i+1)
+                if i == 0:
+                    add_border_around_subfig(ax)
+                axs[i].axis('off')
+                alter_axes(ax)
+                ax.set_xlabel('query document' if i==0 else '{}. response document'.format(i))
+                image = cv2.imread(img, cv2.IMREAD_COLOR)
+                plt.imshow(image)
+
+            title = 'Most similar images found by {}'.format(model)
+            plt.title(title)
+            if outpath != '':
+                if not os.path.exists(outpath + 'png/'):
+                    os.mkdir(outpath + 'png/')
+                if not os.path.exists(outpath + 'png/' + query.iloc[doc_idx].name):
+                    os.mkdir(outpath + 'png/' + query.iloc[doc_idx].name)
+                plt.savefig(outpath + 'png/' + query.iloc[doc_idx].name + '/' + re.sub(' ', '_', title) + '.png', format="png", bbox_inches="tight", dpi=1200)
+            #plt.show()
+
+def add_broder_around_figure(fig):
+    fig.patch.set_linewidth(10)
+    fig.patch.set_edgecolor('blue')
+
+
+
 def main(baseDir:str):
     
-    # res_df = create_sim_docs_log(baseDir=baseDir, num_rep_docs=2000, n_res=10)
-    # save_to_dir(resDir='results/', df=res_df, file_name='query_res.csv')
+    # for num_resp in [3, 5, 10]:
+    #     res_df = create_sim_docs_log(baseDir=baseDir, num_rep_docs=10, n_res=num_resp)
+    #     save_to_dir(resDir='results/', df=res_df, file_name='query_res_{}_resp.csv'.format(num_resp))
 
-    res_df = read_from_dir('results/' + 'query_res.csv')
-    print(res_df)
+    #     visualize_using_venn(res_df, save=True, title='Venn diagram of {} most similar documents on 2048 document corpus'.format(num_resp))
+
+    res_df = read_from_dir('results/' + 'query_res_5_resp.csv')
+    create_query_img(res_df, image_baseDir='/Users/klara/Documents/uni/bachelorarbeit/images', outpath='results/')
+    # res_df = read_from_dir('results/' + 'query_res.csv')
+    # print(res_df)
     
-    res_df = encode_lists(res_df)
-    # visualize_using_venn(res_df, save=True)
+    # res_df = encode_lists(res_df)
+    # # visualize_using_venn(res_df, save=True)
 
-    create_sim_heatmap(res_df, save=True)
+    # create_sim_heatmap(res_df, save=True)
 
